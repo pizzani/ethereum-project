ad5f50e47823e6e4940c1d744da37a1fc66f91bc
Eduardo Pizzani <83318348+pizzani@users.noreply.github.com>
2026-04-11 10:23:10 -0300

    Transaction categories

--- eth/models/transactions_enriched.sql
+++ eth/models/transactions_enriched.sql
@@ -6,5 +6,21 @@ t.from_address,
 t.to_address,
 t.value,
 t.receipt_contract_address,
-t.input
+t.input,
+
+case
+    when t.receipt_contract_address != '' then 'contract_creation'
+    when tt.transaction_hash is not null then 'token_transfer'
+    when t.input = '0x' and t.value > 0 then 'plain_eth_transfer'
+    else 'other'
+end as transaction_category
+
 from {{ source('eth','transactions') }} t
+left join (
+    SELECT
+    transaction_hash,
+    count(*) as token_transfer_count
+    from {{ source('eth','token_transfers') }}
+    group by transaction_hash
+) tt
+on t.hash = tt.transaction_hash
