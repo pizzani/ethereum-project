e1aaed91c7c2e7d716e043a70e11adb92eca4d02
Eduardo Pizzani <83318348+pizzani@users.noreply.github.com>
2026-04-12 17:47:19 -0300

    New folders structure

--- eth/models/marts/eth_activity_per_day.sql
+++ eth/models/marts/eth_activity_per_day.sql
@@ -3,7 +3,7 @@ date,
 transaction_category,
 count(*) as tx_count,
 sum(value)/1e18 sum_eth_value
-from {{ ref('transactions_enriched') }}
+from {{ ref('stg_transactions_enriched') }}
 group by
 date,
 transaction_category
