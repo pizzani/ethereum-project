5db85a22ba0b827943669764d2118c158bc8f4e9
Eduardo Pizzani <83318348+pizzani@users.noreply.github.com>
2026-04-26 12:08:41 -0300

    codegen package

---
+++ eth/analyses/codegen_test.sql
@@ -0,0 +1,16 @@
+{{  codegen.generate_source(
+    schema_name='eth_schema', 
+    database_name= 'eth', 
+    generate_columns= True,
+    include_data_types= False
+    ) }}
+
+{{
+    codegen.generate_model_yaml(
+        [
+            'stg_transactions',
+            'stg_transactions_enriched',
+            'stablecoin_activity_per_day'
+        ]
+    )
+}}
